38ae47fd5a87622659d6c0540f521bd2071f5f20
zanzidaric <zan.502@gmail.com>
2021-10-08 12:53:45 +0200

    PalindromRek

---
+++ PalindromRek/PalindromRek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromRek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalCheck("anabna"));
+            Console.WriteLine(PalCheck("ana"));
+            Console.ReadLine();
+        }
+        static Boolean PalCheck(String a)
+        {
+
+            if (a.Length == 0 || a.Length == 1)
+                return true;
+            if (a[0] == a[a.Length - 1])
+                return PalCheck (a.Substring(1, a.Length - 2));
+            return false;
+                
+            
+        }
+    }
+}
