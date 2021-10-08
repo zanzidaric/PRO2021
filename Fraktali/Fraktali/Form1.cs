b413e3bec77dd07c1fc6eef1d2965a26be0aec95
zanzidaric <zan.502@gmail.com>
2021-10-08 13:27:34 +0200

    Fraktali

---
+++ Fraktali/Fraktali/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fraktali
+{
+    public partial class Form1 : Form
+    {
+        private double x;   //pozicija zelve
+        private double y;   
+        private double alfa;    //kot glede na os x
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int risaniX(double xr)
+        {
+            return (int)Math.Round(this.Width * xr);
+        }
+        private int risaniY(double yr)
+        {
+            return (int)Math.Round(this.Height - this.Height * yr);
+        }
+        public void ObratLevo(double kot)
+        {
+            alfa = alfa + kot;
+        }
+        public void Premik(double d, Graphics g)
+        {
+            double stariX = x;
+            double stariY = y;
+            x = x + Math.Cos(alfa*Math.PI/180);
+            y = y + Math.Sin(alfa*Math.PI/180);
+            int x1 = risaniX(stariX);
+            int y1 = risaniY(stariY);
+            int x2 = risaniX(x);
+            int y2 = risaniY(y);
+            //Pen p = new Pen(Color.Black, 3);
+            g.DrawLine(new Pen(Color.Black, 3), x1, y1, x2, y2);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            x = 0.5;y = 0.5;alfa = 0;
+            Premik(0.5,g);
+
+        }
+    }
+}
